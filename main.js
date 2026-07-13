ca963b5ef5ab28547999beb83a429e081a2df1d3
hdavis094 <hdavis094@gmail.com>
2026-07-13 14:12:53 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $.getJSON('data.json').done(function(product) {
+        
+
+    });
+});
+
+// Fetching data from a public internet API
+//$.getJSON('https://api.weatherapi.com/v1/current.json?key=123&q=London')
